fed79a1fd3fd35626ab9a59e76d2c4c19f921938
Salma-El-Alaoui <s.elalaouitalibi@gmail.com>
2014-10-03 00:58:10 +0200

    forgot to add this file in previous commit

--- sched.c
+++ sched.c
@@ -6,3 +6,23 @@ void init_ctx(struct ctx_s* ctx, func_t f, unsigned int stack_size) {
   (*ctx).pc = f;
   (*ctx).sp = phyAlloc_alloc(STACK_SIZE);
 }
+
+void switch_to(struct ctx_s* ctx) {
+ int functionSp, functionPc;
+
+//TODO : 
+//Save the context
+//
+  // we save the previous context -> we get the values stored in sp and lr : TODO
+  current_ctx->sp = functionSp;
+  current_ctx->pc = functionPc;
+
+  //we change the context
+  current_ctx = ctx;
+
+  functionSp = current_ctx->sp;
+  functionPc = current_ctx->pc;
+  //TODO: we should move  functionSp to sp and functionPc to pc
+
+ 
+}
