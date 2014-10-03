bfd6f659e554d188a0f018e80ff5a3d7be7e5545
el Alaoui Talibi Salma <selalaouit@if501-208-09.insa-lyon.fr>
2014-10-03 11:56:51 +0200

    added function to initialize the PCB

--- sched.c
+++ sched.c
@@ -2,11 +2,50 @@
 #include "sched.h"
 #include "phyAlloc.h"
 
-void init_ctx(struct ctx_s* ctx, func_t f, unsigned int stack_size) {
-  ctx->pc = f;
-  ctx->sp= (int*) phyAlloc_alloc(stack_size)+ stack_size/sizeof(int)-1;
+struct pcb_s * current_pcb = NULL;
+
+void init_pcb(struct pcb_s* pcb, func_t f, void* args, unsigned int stack_size) {
+	//Initialize process state to READY 
+	pcb->pstate = process_state.READY;
+	
+	//Assign function and arguments
+	pcb->function = f;
+	pcb->args =  args;
+ 
+	//PC  & SP
+	pcb->pc = f;
+  	pcb->sp= (int*) phyAlloc_alloc(stack_size)+ (stack_size -1)/sizeof(int);
+
+	//Next & previous to NULL
+	pcb->next = NULL;
+	pcb->previous = NULL;
 }
 
+void create_process(func_t f, void* args, unsigned int stack_size){
+	pcb_s* pcb = malloc(sizeof(pcb_s));
+	
+	//Insert into linked list
+	if (current_pcb == NULL)//Empty list
+	{
+		current_pcb = pcb;
+		pcb->next = pcb;
+		pcb->previous = pcb;
+	}
+	else
+	{
+		
+		pcb_s->next= current_pcb->next;
+		pcb_s->previous=current_pcb;
+		current_pcb->next = pcb_s;
+		current_pc= pcb_s;
+	}
+ 
+	//Initialize PCB
+	init_pcb(pcb_s, f, args, stack_size);
+	 
+	
+
+}
 void __attribute__ ((naked)) switch_to(struct ctx_s* ctx) {
 	
 	//Save the context
--- sched.h
+++ sched.h
@@ -13,16 +13,28 @@
 
 typedef void (*func_t) (void);
 
-struct ctx_s {
- int* pc;
- int* sp;
+typedef enum {NEW, READY, RUNNING, WAITING, TERMINATED} process_state;
+
+struct pcb_s {
+	process_state pstate;
+	func_t function;
+	void * args;
+	int* pc;
+	int* sp;
+	pcb_s * previous;
+	pcb_s * next;
+
 };
+
 
 struct ctx_s* current_ctx;
 
 void init_ctx(struct ctx_s* ctx, func_t f, unsigned int stack_size);
 
 void __attribute__ ((naked))switch_to(struct ctx_s* ctx);
+
+int create_process(func_t f, void *args, unsigned int stack_size);
+void __attribute__ ((naked)) ctx_switch();
 
 
 #endif
